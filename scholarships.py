84753618f81382d84454ab6a12951f3b6b676430
MrXlVii <nale2892@gmail.com>
2017-03-17 20:35:56 -0400

    full rough draft brute force

--- scholarships.py
+++ scholarships.py
@@ -1,12 +1,15 @@
 """Rough draft brute force solutions"""
 
+import numpy as np
 
-def findGreatest(self, matrix, k):
+def findGreatest(self, lists, k):
     """Takes in matrix, finds k consecutive values that multiply to largest value"""
 
     sequence = [k]
     maxTotal = 0
     tmp = 0
+
+    matrix = np.array(lists)
 
     # Horizontal span O(n^2)
     for row in matrix:
@@ -15,7 +18,6 @@ def findGreatest(self, matrix, k):
                 tmp *= row[i:(i+k)]
             else:
                 tmp = 0
-
         if tmp > maxTotal:
             maxTotal = tmp
             sequence = row[i:(i+k)]
@@ -31,7 +33,6 @@ def findGreatest(self, matrix, k):
                 tmp *= column[i:(i+k)]
             else:
                 tmp = 0
-
         if tmp > maxTotal:
             maxTotal = tmp
             sequence = column[i:(i+k)]
@@ -39,8 +40,23 @@ def findGreatest(self, matrix, k):
             pass
 
     # L-diag, R-diag
+    diags = [matrix[::-1,:].diagonal(i) for i in range(-matrix.shape[0]+1, matrix.shape[1])]
+    diags.extend(matrix.diagonal(i) for i in range(a.shape[1]-1,-a.shape[0],-1))
 
+    diags = [n.tolist() for n in diags]
 
-def zip_uneven():
-    """ Recursive function to map diagnols"""
-    pass
+    # Diag sub-array span O(n^2)
+    for seq in diags:
+        if len(seq) >= k:
+            for i in range(len(seq)):
+                if (i+k-1) <= len(seq):
+                    tmp *= seq[i:(i+k)]
+                else:
+                    tmp = 0
+            if tmp > maxTotal:
+                maxTotal = tmp
+                sequence = seq[i:(i+k)]
+            else:
+                pass
+        else:
+            pass
