55f8b1d4dceec1da949515e3ec4559d65a37a18c
MrXlVii <nale2892@gmail.com>
2017-03-17 21:31:03 -0400

    cleaned up code

--- scholarships.py
+++ scholarships.py
@@ -3,50 +3,31 @@
 import numpy as np
 
 def findGreatest(self, lists, k):
+    matrix = np.array(lists)
+
+    # Horizontal span O(n^2)
+    horizontal = spanLists(matrix, k)
+
+    # Vertical span O(n^2)
+    vertMat = map(list, zip(*matrix)) # Vertical colums work as rows
+    vertical = spanLists(vertMat, k)
+
+    # L-diag, R-diag
+    diags = [matrix[::-1, :].diagonal(i) for i in range(-matrix.shape[0]+1, matrix.shape[1])]
+    diags.extend(matrix.diagonal(i) for i in range(matrix.shape[1]-1, -matrix.shape[0], -1))
+    diags = [n.tolist() for n in diags]
+
+    diagnol = spanLists(diags, k)
+
+def spanLists(self, lists, k):
     """Takes in matrix, finds k consecutive values that multiply to largest value"""
 
     sequence = [k]
     maxTotal = 0
     tmp = 0
+    response = {}
 
-    matrix = np.array(lists)
-
-    # Horizontal span O(n^2)
-    for row in matrix:
-        for i in range(len(row)):
-            if (i+k-1) <= len(row):
-                tmp *= row[i:(i+k)]
-            else:
-                tmp = 0
-        if tmp > maxTotal:
-            maxTotal = tmp
-            sequence = row[i:(i+k)]
-        else:
-            pass
-
-    # Vertical span O(n^2)
-    vertMat = map(list, zip(*matrix)) # Vertical colums work as rows
-
-    for column in vertMat:
-        for i in range(len(column)):
-            if (i+k-1) <= len(column):
-                tmp *= column[i:(i+k)]
-            else:
-                tmp = 0
-        if tmp > maxTotal:
-            maxTotal = tmp
-            sequence = column[i:(i+k)]
-        else:
-            pass
-
-    # L-diag, R-diag
-    diags = [matrix[::-1,:].diagonal(i) for i in range(-matrix.shape[0]+1, matrix.shape[1])]
-    diags.extend(matrix.diagonal(i) for i in range(a.shape[1]-1,-a.shape[0],-1))
-
-    diags = [n.tolist() for n in diags]
-
-    # Diag sub-array span O(n^2)
-    for seq in diags:
+    for seq in lists:
         if len(seq) >= k:
             for i in range(len(seq)):
                 if (i+k-1) <= len(seq):
@@ -60,3 +41,8 @@ def findGreatest(self, lists, k):
                 pass
         else:
             pass
+    
+    response['sequence'] = sequence
+    response['total'] = maxTotal
+
+    return response
---
+++ tests/testScholarships.py
@@ -0,0 +1,10 @@
+import unittest
+import scholarships
+
+class TestMethods(unittest.TestCase):
+
+    test_FindGreatest(self):
+        pass
+
+    test_SpanLists(self):
+        pass
