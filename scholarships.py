105e401724f517f5241f4b188f1bbbca602ab777
MrXlVii <nale2892@gmail.com>
2017-03-20 21:32:34 -0400

    algorithm passes basic example

--- scholarships.py
+++ scholarships.py
@@ -1,16 +1,33 @@
 """Rough draft brute force solutions"""
 
 import numpy as np
+import requests
 
-def findGreatest(self, lists, k):
+
+def main():
+    """
+    req = requests.get(url)  # Not sure what url to get
+    req.json()
+
+    scholarships = req[data]
+    total = self.findGreatest(scholarships, 11)
+
+    r = requests.post(url, data=total)
+    """
+
+
+def findGreatest(lists, k):
     matrix = np.array(lists)
+    print matrix
 
-    # Horizontal span O(n^2)
-    horizontal = spanLists(matrix, k)
+    # Horizontal span
+    horizontal = spanLists(lists, k)
+    print horizontal
 
-    # Vertical span O(n^2)
-    vertMat = map(list, zip(*matrix)) # Vertical colums work as rows
+    # Vertical span
+    vertMat = map(list, zip(*matrix))  # Vertical colums work as rows
     vertical = spanLists(vertMat, k)
+    print vertical
 
     # L-diag, R-diag
     diags = [matrix[::-1, :].diagonal(i) for i in range(-matrix.shape[0]+1, matrix.shape[1])]
@@ -18,11 +35,23 @@ def findGreatest(self, lists, k):
     diags = [n.tolist() for n in diags]
 
     diagnol = spanLists(diags, k)
+    print diagnol
 
-def spanLists(self, lists, k):
+    great = 0
+    for val in [horizontal, vertical, diagnol]:
+        if val['total'] > great:
+            great = val['total']
+            correct = val
+        else:
+            pass
+
+    return correct
+
+
+def spanLists(lists, k):
     """Takes in matrix, finds k consecutive values that multiply to largest value"""
-
-    sequence = [k]
+    print lists
+    sequence = []
     maxTotal = 0
     tmp = 0
     response = {}
@@ -31,18 +60,22 @@ def spanLists(self, lists, k):
         if len(seq) >= k:
             for i in range(len(seq)):
                 if (i+k-1) <= len(seq):
-                    tmp *= seq[i:(i+k)]
+                    tmp = np.prod(seq[i:(i+k)])
+                    if tmp > maxTotal:
+                        maxTotal = tmp
+                        del sequence[:]
+                        sequence.append(seq[i:(i+k)])
+                    else:
+                        pass
                 else:
                     tmp = 0
-            if tmp > maxTotal:
-                maxTotal = tmp
-                sequence = seq[i:(i+k)]
-            else:
-                pass
         else:
             pass
-    
-    response['sequence'] = sequence
-    response['total'] = maxTotal
 
+    print maxTotal
+    response['sequence'] = sequence[0]
+    response['total'] = maxTotal
     return response
+
+if __name__ == '__main__':
+    main()
--- tests/testScholarships.py
+++ tests/testScholarships.py
@@ -3,8 +3,10 @@ import scholarships
 
 class TestMethods(unittest.TestCase):
 
-    test_FindGreatest(self):
-        pass
+    def test_FindGreatest(self):
+        basic = [[1,2,3,4,5], [1,1,2,3,5], [3,4,5,5,5], [3,4,5,9,5], [1,1,5,5,25]]
+        s = scholarships.findGreatest(basic, 3)
+        self.assertEqual(s, {"sequence": [5,9,25], "total": 1125})
 
-    test_SpanLists(self):
+    def test_SpanLists(self):
         pass
