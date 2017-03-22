0436a75e0e4c0daad97693cf4950a9a2fd36ddf6
MrXlVii <nale2892@gmail.com>
2017-03-21 23:50:45 -0400

    added basic REST API

--- scholarships.py
+++ scholarships.py
@@ -1,75 +1,89 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 import numpy as np
 
 scholarships = Flask(__name__)
 
 data = [[1,2,3,4,5], [1,1,2,3,5], [3,4,5,5,5], [3,4,5,9,5], [1,1,5,5,25]]  # Our pseudo-database
+max_scholarship = []
+
 
 @scholarships.route('/', methods=['GET'])
-def get_data():
+def getData():
     return jsonify({'data': data})
 
+@scholarships.route('/max_scholarship', methods=['GET'])
+def getMaxScholarship():
+    return jsonify(max_scholarship)
 
-def findGreatest(lists, k):
+@scholarships.route('/max_scholarship', methods=['POST'])
+def maxScholarship():
+    ms = MaxScholarship()
+    vals = ms.findGreatest(data, 3)
+    max_scholarship.append(vals)
+    return jsonify(max_scholarship), 201
 
-    matrix = np.array(lists)
-    print matrix
-
-    # Horizontal span
-    horizontal = spanLists(lists, k)
-    print horizontal
-
-    # Vertical span
-    vertMat = map(list, zip(*matrix))  # Vertical columns work as rows
-    vertical = spanLists(vertMat, k)
-    print vertical
-
-    # L-diag, R-diag
-    diags = [matrix[::-1, :].diagonal(i) for i in range(-matrix.shape[0]+1, matrix.shape[1])]
-    diags.extend(matrix.diagonal(i) for i in range(matrix.shape[1]-1, -matrix.shape[0], -1))
-    diags = [n.tolist() for n in diags]
-
-    diagnol = spanLists(diags, k)
-    print diagnol
-
-    great = 0
-    for val in [horizontal, vertical, diagnol]:
-        if val['total'] > great:
-            great = val['total']
-            correct = val
-        else:
-            pass
-
-    return correct
+@scholarships.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-def spanLists(lists, k):
-    """Takes in matrix, finds k consecutive values that multiply to largest value"""
-    sequence = []
-    maxTotal = 0
-    tmp = 0
-    response = {}
+class MaxScholarship(object):
 
-    for seq in lists:
-        if len(seq) >= k:
-            for i in range(len(seq)):
-                if (i+k-1) <= len(seq):
-                    tmp = np.prod(seq[i:(i+k)])
-                    if tmp > maxTotal:
-                        maxTotal = tmp
-                        del sequence[:]
-                        sequence.append(seq[i:(i+k)])
+    def findGreatest(self, lists, k):
+        matrix = np.array(lists)
+
+        # Horizontal span
+        horizontal = self.spanLists(lists, k)
+
+        # Vertical span
+        vertMat = map(list, zip(*matrix))  # Vertical columns work as rows
+        vertical = self.spanLists(vertMat, k)
+
+        # L-diag, R-diag
+        diags = [matrix[::-1, :].diagonal(i) for i in range(-matrix.shape[0]+1, matrix.shape[1])]
+        diags.extend(matrix.diagonal(i) for i in range(matrix.shape[1]-1, -matrix.shape[0], -1))
+        diags = [n.tolist() for n in diags]
+
+        diagnol = self.spanLists(diags, k)
+
+        great = 0
+        for val in [horizontal, vertical, diagnol]:
+            if val['total'] > great:
+                great = val['total']
+                correct = val
+            else:
+                pass
+
+        return correct
+
+
+    def spanLists(self, lists, k):
+        """Takes in matrix, finds k consecutive values that multiply to largest value"""
+        sequence = []
+        maxTotal = 0
+        tmp = 0
+        response = {}
+
+        for seq in lists:
+            if len(seq) >= k:
+                for i in range(len(seq)):
+                    if (i+k-1) <= len(seq):
+                        tmp = np.prod(seq[i:(i+k)])
+                        if tmp > maxTotal:
+                            maxTotal = tmp
+                            del sequence[:]
+                            sequence.append(seq[i:(i+k)])
+                        else:
+                            pass
                     else:
-                        pass
-                else:
-                    tmp = 0
-        else:
-            pass
+                        tmp = 0
+            else:
+                pass
 
-    print maxTotal
-    response['sequence'] = sequence[0]
-    response['total'] = maxTotal
-    return response
+        print maxTotal
+        response['sequence'] = sequence[0]
+        response['total'] = maxTotal
+        return response
 
 if __name__ == '__main__':
     scholarships.run(debug=True)
